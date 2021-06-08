aeaa8c69fab146f9a6a385314120a9b7a81a8d5d
Raul <raul.lopezm@gmail.com>
2021-06-08 12:26:15 +0200

    Ej1. Lectura de fichero JSON

--- Ej1/index.js
+++ Ej1/index.js
@@ -1,16 +1,19 @@
+import fs from 'fs';
 import express from 'express';
 
 const app = express();
+const file = fs.readFileSync('./user.json');
+const userInfo = JSON.parse(file);
 
 app.get('/user', (req, res) => {
     res.json({
-        name: 'User1',
-        age: 35,
+        name: userInfo.name,
+        age: userInfo.age,
         url: req.protocol + "://" + req.get('host') + req.originalUrl,
     });
 });
 
 app.listen(3000, () => {
-    console.log('Listen en puerto 3000');
+    console.log("Server on port 3000");
 });
 
