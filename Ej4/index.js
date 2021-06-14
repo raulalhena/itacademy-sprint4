2151fc4503189a62e513c7f0fe881ba3d36e24d6
Raul <raul.lopezm@gmail.com>
2021-06-14 12:26:41 +0200

    Cambio a uso de basic-auth

--- Ej4/index.js
+++ Ej4/index.js
@@ -1,19 +1,20 @@
 import express from 'express';
-import basicAuth from 'express-basic-auth';
+import basicAuth from 'basic-auth';
 
 const app = express();
 
-app.use(basicAuth({
-    users: {
-        'user1': 'pass1'
-    },
-    challenge: true
-}));
-
-app.get('/', (req, res) =>{
-    res.send("Hey people!");
+app.use((req, res, next) => {
+    
+    next();
 });
 
+function noAuth(req) {
+    return req.auth ? ('Credentials ' + req.auth.user + ':' + req.auth.password + ' rejected') : 'No credentials provided';
+}
+
+app.get('/', (req, res) =>{
+    res.send('Hey, acceso correcto!');
+});
 
 app.listen(3000, () => {
     console.log("Server listening on port 3000");
